54af235e622bbe1340898306826e2c03fc6fa173
igor paim <igorp984@gmail.com>
2018-01-20 22:49:18 -0500

    fazendo a conexao com o banco pra funcionar no heroku

--- app/_inc/config.php
+++ app/_inc/config.php
@@ -1,10 +1,11 @@
 <?php
 define('SERVER', 'D');
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_HOST', 'cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('MYSQL_PORT', '3306');
-define('MYSQL_USER', 'selecao_fullstack');
-define('MYSQL_PASS', 'barcelona');
-define('MYSQL_BASE', 'selecao_fullstack');
+define('MYSQL_USER', 'ou1amd3du8xzowsb');
+define('MYSQL_PASS', 's3szx0nymmwlrm89');
+define('MYSQL_BASE', 'u1gly9alojbmyuy3');
 define('MYSQL_CHARSET', 'utf8');
 define('MYSQL_TIMEZONE', '-03:00');
 define('PHP_TIMEZONE', 'America/Bahia');
+
