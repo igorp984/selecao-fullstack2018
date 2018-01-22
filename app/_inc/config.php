34f52cb47db36ef39f486e1fb7df0b7de859526c
Mauricio Marques <mauriciommfsa@gmail.com>
2018-01-22 19:39:19 -0300

    Config de banco

--- app/_inc/config.php
+++ app/_inc/config.php
@@ -1,10 +1,20 @@
 <?php
+// define('SERVER', 'D');
+// define('MYSQL_HOST', 'localhost');
+// define('MYSQL_PORT', '3306');
+// define('MYSQL_USER', 'selecao_fullstack');
+// define('MYSQL_PASS', 'barcelona');
+// define('MYSQL_BASE', 'selecao_fullstack');
+// define('MYSQL_CHARSET', 'utf8');
+// define('MYSQL_TIMEZONE', '-03:00');
+// define('PHP_TIMEZONE', 'America/Bahia');
+
 define('SERVER', 'D');
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_HOST', 'yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('MYSQL_PORT', '3306');
-define('MYSQL_USER', 'selecao_fullstack');
-define('MYSQL_PASS', 'barcelona');
-define('MYSQL_BASE', 'selecao_fullstack');
+define('MYSQL_USER', 'uwmgajx1hvt5ndi9');
+define('MYSQL_PASS', 'grocztiaxw2y6rte');
+define('MYSQL_BASE', 'bl6y6fsmor5av1ht');
 define('MYSQL_CHARSET', 'utf8');
 define('MYSQL_TIMEZONE', '-03:00');
 define('PHP_TIMEZONE', 'America/Bahia');
